a8f946e2a176149fd75910ffc918197ad4b19f43
zeft201 <126609512+zeft201@users.noreply.github.com>
2023-04-18 18:16:26 +0100

    Create Button.cs

---
+++ Button.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Animation Button;
+    public Animation toOpen;
+    
+    void start()
+    {
+        button.enable = false;
+    }
+    
+    private void onTriggerEnter(Collider other)
+    {
+        button.enabled = true;
+        toOpen.enabled = true;
+    }
+}    
